512b156286cb418a83482d248393cba4df7d62ac
Varanio117 <spartanxbox360@gmail.com>
2021-04-14 19:15:04 +0200

    .

---
+++ src/login.php
@@ -0,0 +1,44 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title> VMusic!</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<header>
+    VMusic!
+</header>
+
+<body>
+  
+  <form method="post" action="login.php">
+  	<h1>Iniciar sesión</h1>   
+  	<?php include('errors.php'); ?>
+
+	
+  	<div class="input-group">
+  		<label>Usuario:</label>
+  		<input type="text" name="username" id="username" placeholder="Nombre de usuario">
+  	</div>
+  	<div class="input-group">
+  		<label>Contraseña</label>
+  		<input type="password" name="password" id="password" placeholder="Introduce contraseña">
+  	</div>
+	<div class="remember">  
+	  <p>
+          <input type="checkbox" name="remember" id="remember">
+          <label for="remember">Recuérdame</label>
+       </p>
+	</div>   
+	<div class="login">
+		<span><a href="remember.php">Contraseña olvidada</a></span>
+  		<button type="submit" class="btn" name="login_user">Entrar</button>
+  	</div>
+  	<p>
+  		¿No es miembro todavía? <a href="register.php">Regístrese aquí</a>
+  	</p>
+  </form>
+</body>
+</html>
---
+++ src/register.php
@@ -0,0 +1,37 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Registro</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	
+  <form method="post" action="register.php">
+  	<h1>Registro</h1> 
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  	  <label>Nombre de Usuario</label>
+  	  <input type="text" name="username" value="<?php echo $username; ?>" placeholder="Elija un nombre de usuario">
+  	</div>
+  	<div class="input-group">
+  	  <label>Email</label>
+  	  <input type="email" name="email" value="<?php echo $email; ?>" placeholder="Introduzca su correo electrónico">
+  	</div>
+  	<div class="input-group">
+  	  <label>Contraseña</label>
+  	  <input type="password" name="password_1" placeholder="Elija una contraseña">
+  	</div>
+  	<div class="input-group">
+  	  <label>Confirmar contraseña</label>
+  	  <input type="password" name="password_2" placeholder="Vuelva a introducir contraseña">
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Registrarme</button>
+  	</div>
+  	<p>
+  		¿Ya está registrado? <a href="login.php">Inicie sesión</a>
+  	</p>
+  </form>
+</body>
+</html>
